4a5ef76cb258e2c5290a7803bd9c2f40ff964cbd
ApZero <stefan_kufeld@hotmail.com>
2026-07-23 22:24:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_NAME en cada deploy para forzar la actualización en Android/Chrome
-const CACHE_NAME = "vianda-v1";
+const CACHE_NAME = "vianda-v2";
 const ASSETS = [
   "./",
   "./index.html",
