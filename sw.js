93f595b219ad91b3a12eb2c6ef8becf431d5f92c
ApZero <stefan_kufeld@hotmail.com>
2026-07-23 21:47:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Bump CACHE_NAME en cada deploy para forzar la actualización en Android/Chrome
+const CACHE_NAME = "vianda-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/seed-ingredients.js",
+  "./js/storage.js",
+  "./js/nutrition.js",
+  "./js/score.js",
+  "./js/backup.js",
+  "./js/ui-ingredients.js",
+  "./js/ui-batches.js",
+  "./js/ui-freezer.js",
+  "./js/ui-stats.js",
+  "./js/ui-home.js",
+  "./js/app.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((resp) => {
+          if (resp && resp.status === 200) {
+            const clone = resp.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return resp;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
